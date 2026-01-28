28b36d038353db6fdaf0a7d1d703d73e9d2f32c0
Tan Wei Jian <60432453+wj-tan@users.noreply.github.com>
2026-01-28 15:36:14 +0800

    Update batchJobRights.ps1

--- batchJobRights.ps1
+++ batchJobRights.ps1
@@ -1,23 +1,22 @@
-# Export current local security policy
-secedit.exe /export /cfg C:\Temp\secpol.inf /areas USER_RIGHTS
+# Get the SID for LAB\svc_taskscheduler
+$Account = 'LAB\svc_taskscheduler'
+$SID = (New-Object System.Security.Principal.NTAccount($Account)).
+       Translate([System.Security.Principal.SecurityIdentifier]).Value
 
-# Get the SID for LAB\Administrator
-$SID = 'LAB\Administrator' | ForEach-Object {
-    (New-Object System.Security.Principal.NTAccount($_)).Translate([System.Security.Principal.SecurityIdentifier]).Value
-}
-Write-Host "SID for LAB\Administrator is $SID"
+Write-Host "SID for $Account is $SID"
 
 # Create a minimal INF file to grant SeBatchLogonRight
-$Policy = @"
+$Policy = @'
 [Unicode]
 Unicode=yes
 [Version]
 signature="$CHICAGO$"
 Revision=1
 [Privilege Rights]
-SeBatchLogonRight = *S-1-5-32-544,*S-1-5-32-551,*S-1-5-32-559,*S-1-5-32-568,*$SID
-"@
-$Policy | Set-Content C:\Temp\grant_batchlogon.inf -Encoding ASCII
+SeBatchLogonRight = *S-1-5-32-544,*S-1-5-32-551,*S-1-5-32-559,*S-1-5-32-568,
+'@ + "*$SID"
+
+$Policy | Out-File C:\Temp\grant_batchlogon.inf -Encoding ASCII -Force
 
 # Import & apply new policy
 secedit.exe /import /db C:\Temp\grant_batchlogon.sdb /cfg C:\Temp\grant_batchlogon.inf
